e855c330e5d2ec66e068c52a3c05d6c1c70f15e0
devGuig <gley392@gmail.com>
2019-02-28 16:05:25 +0100

    new modif

--- gameproject/src/App.js
+++ gameproject/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          
+          <h1>On va y arriver !!</h1>
         </header>
       </div>
     );
