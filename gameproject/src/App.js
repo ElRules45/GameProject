4bb7eec7f1ee6af3dfc8ea791fce228d3dfab36b
devGuig <gley392@gmail.com>
2019-03-06 14:48:25 +0100

    jules tu fais chier

--- gameproject/src/App.js
+++ gameproject/src/App.js
@@ -5,45 +5,47 @@ import './App.scss';
 class App extends Component {
   render() {
     return (
-      <section className="hero is-info is-fullheight">
-        <div className="hero-head">
-          <nav className="navbar">
-            <div className="container">
-              <div className="navbar-brand">
-                <span className="navbar-burger burger" data-target="navbarMenuHeroB">
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </span>
-              </div>
-              <div id="navbarMenuHeroB" className="navbar-menu">
-                <div className="navbar-end">
-                  <a className="navbar-item is-active">
-                    Home
-                  </a>
-                  <a className="navbar-item">
-                    Examples
-                  </a>
-                  <a className="navbar-item">
-                    Documentation
-                  </a>
+      <body>
+        <section className="hero is-info is-fullheight">
+          <div className="hero-head">
+            <nav className="navbar">
+              <div className="container">
+                <div className="navbar-brand">
+                  <span className="navbar-burger burger" data-target="navbarMenuHeroB">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </span>
+                </div>
+                <div id="navbarMenuHeroB" className="navbar-menu">
+                  <div className="navbar-end">
+                    <a className="navbar-item is-active">
+                      Home
+                    </a>
+                    <a className="navbar-item">
+                      Examples
+                    </a>
+                    <a className="navbar-item">
+                      Documentation
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
-          </nav>
-        </div>
-
-        <div className="hero-body">
-          <div className="container has-text-centered">
-            <p className="title is-1">
-              Select your GAME
-            </p>
-            <p className="subtitle is-4">
-              ESD GAME
-            </p>
+            </nav>
           </div>
-        </div>
-      </section>
+
+          <div className="hero-body">
+            <div className="container has-text-centered">
+              <p className="title is-1">
+                Select your GAME
+              </p>
+              <p className="subtitle is-4">
+                ESD GAME
+              </p>
+            </div>
+          </div>
+        </section>
+      </body>
     );
   }
 }
