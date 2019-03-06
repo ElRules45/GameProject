688c38f6a545f63e13b9b7a6905254919f958033
devGuig <gley392@gmail.com>
2019-03-06 13:31:31 +0100

    change header bulma

--- gameproject/src/App.js
+++ gameproject/src/App.js
@@ -5,11 +5,53 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>ESD GAME</h1>
-        </header>
-      </div>
+      <section className="hero is-info is-large">
+        <div className="hero-head">
+          <nav className="navbar">
+            <div className="container">
+              <div className="navbar-brand">
+                <span className="navbar-burger burger" data-target="navbarMenuHeroB">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </span>
+              </div>
+              <div id="navbarMenuHeroB" className="navbar-menu">
+                <div className="navbar-end">
+                  <a className="navbar-item is-active">
+                    Home
+                  </a>
+                  <a className="navbar-item">
+                    Examples
+                  </a>
+                  <a className="navbar-item">
+                    Documentation
+                  </a>
+                  <span className="navbar-item">
+                    <a className="button is-info is-inverted">
+                      <span className="icon">
+                        <i className="fab fa-github"></i>
+                      </span>
+                      <span>Download</span>
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <p className="title">
+              Title
+            </p>
+            <p className="subtitle">
+              Subtitle
+            </p>
+          </div>
+        </div>
+      </section>
     );
   }
 }
